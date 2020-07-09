e86e981963db83f51bbeb1e83e65140332780df3
Do Nguyen <do.nguyen@gmail.com>
2020-07-09 14:19:19 +1000

    m

--- singleVM_infoblox/main.tf
+++ singleVM_infoblox/main.tf
@@ -140,10 +140,6 @@ variable "vm_1_ipv4_gateway" {
   description = "IPv4 gateway for vNIC configuration"
 }
 
-variable "vm_1_ipv4_address" {
-  description = "IPv4 address for vNIC configuration"
-}
-
 variable "vm_1_ipv4_prefix_length" {
   description = "IPv4 prefix length for vNIC configuration. The value must be a number between 8 and 32"
 }
